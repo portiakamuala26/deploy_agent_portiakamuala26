167c6d0a0cb643a09e651be50adb1daee7e56ecf
portiakamuala26 <p.kamuala@alustudent.com>
2026-06-11 00:29:46 -0700

    Add project name input handling

--- setup_project.sh
+++ setup_project.sh
@@ -3,6 +3,9 @@
 # Automated Project Bootstrapping Script
 # Creates attendance tracker project structure
 
+# Get project name
 read -p "Enter project name: " name
 
 PROJECT="attendance_tracker_$name"
+
+
